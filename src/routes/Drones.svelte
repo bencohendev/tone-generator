6b9fddcb1259a2487fa0fb6f7003f30aca9a0440
bencohendev <ben@bencohen.dev>
2021-01-11 13:14:02 -0500

    added all drones and key control

--- src/routes/Drones.svelte
+++ src/routes/Drones.svelte
@@ -1,12 +1,35 @@
 <script>
+    let keys = [
+        "a",
+        "bb",
+        "b",
+        "c",
+        "db",
+        "d",
+        "eb",
+        "e",
+        "f",
+        "gb",
+        "g",
+        "ab",
+    ];
+
+    let selectedKey = "ab";
 </script>
 
 <style lang="scss">
 </style>
 
 <section>
+    <div>
+        {#each keys as key}
+            <button on:click={() => (selectedKey = key)}>{key}</button>
+        {/each}
+    </div>
+
     <!-- svelte-ignore a11y-media-has-caption -->
-    <audio controls src="/audio/ab.mp3">
+
+    <audio controls src="/audio/{selectedKey}.mp3">
         Your browser does not support the
         <code>audio</code>
         element.
