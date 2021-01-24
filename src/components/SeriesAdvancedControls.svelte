4262fcc4c856ac233e0cbfd36b75dc57999f4484
bencohendev <ben@bencohen.dev>
2021-01-24 17:56:46 -0500

    fixed augmented scale

--- src/components/SeriesAdvancedControls.svelte
+++ src/components/SeriesAdvancedControls.svelte
@@ -120,7 +120,7 @@
                 modeSetter = [0, 1, 3, 4, 6, 7, 9, 10];
                 break;
             case "aug":
-                modeSetter = [0, 4, 8];
+                modeSetter = [0, 2, 4, 6, 8, 10, 12];
                 break;
         }
         return modeSetter;
