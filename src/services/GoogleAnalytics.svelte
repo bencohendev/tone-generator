3baccd5e765cc1a988231c299f5f30870df1695f
bencohendev <ben@bencohen.dev>
2021-01-28 16:54:21 -0500

    page tags

---
+++ src/services/GoogleAnalytics.svelte
@@ -0,0 +1,12 @@
+
+<script>
+  import { stores } from '@sapper/app';
+  const { page } = stores();
+  $: {
+    if (typeof gtag !== "undefined"){
+      gtag("config", process.env.GA_TAG, {
+        page_path: $page.path
+      });
+    }
+  }
+</script>
