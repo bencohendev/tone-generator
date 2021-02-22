adc7e84641b8320c06dd356380a38da996e8d7ea
bencohendev <ben@bencohen.dev>
2021-02-22 10:18:30 -0500

    working before major cleanup

--- src/components/Tuner.svelte
+++ src/components/Tuner.svelte
@@ -2,57 +2,124 @@
     import { PitchDetector } from "pitchy";
     import { audioCtx, allPitches } from "../store";
     import { onMount } from "svelte";
+    //  import Gauge from "gaugeJS";
 
     let showTuner = false;
+    let isInTune = false;
+    let Gauge;
+    let testVal = 0;
     let tunerElem;
+    let gauge;
+    let gaugeDemo;
     let closestPitch;
     let cents;
     let tunerObj = {};
     let freq;
-    onMount(() => {
-        audioCtx.set(new (window.AudioContext || window.webkitAudioContext)());
+    var opts = {
+        angle: 0, // The span of the gauge arc
+        lineWidth: 0.14, // The line thickness
+        radiusScale: 1, // Relative radius
+        pointer: {
+            length: 0.6, // // Relative to gauge radius
+            strokeWidth: 0.015, // The thickness
+            color: "#000000", // Fill color
+        },
+        limitMax: false, // If false, max value increases automatically if value > maxValue
+        limitMin: false, // If true, the min value of the gauge will be fixed
+        colorStart: "#eb4034", // Colors
+        colorStop: "#eb4034", // just experiment with them
+        percentColors: [
+            [0.0, "#ed1000"],
+            [0.5, "#059400"],
+            [1.0, "#ed1000"],
+        ],
+        staticLabels: {
+            font: "10px sans-serif", // Specifies font
+            labels: [-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50], // Print labels at these values
+            color: "#000000", // Optional: Label text color
+            fractionDigits: 0, // Optional: Numerical precision. 0=round off.
+        },
+        renderTicks: {
+            divisions: 10,
+            divWidth: 1.1,
+            divLength: 0.7,
+            divColor: "#333333",
+            subDivisions: 10,
+            subLength: 0.5,
+            subWidth: 0.6,
+            subColor: "#666666",
+        },
+        strokeColor: "#E0E0E0", // to see which ones work best for you
+        generateGradient: true,
+        highDpiSupport: true, // High resolution support
+    };
+
+    onMount(async () => {
+        const module = await import("../libs/gauge-js/dist/gauge");
+        Gauge = module.default.Gauge;
     });
-    //  Math.round(1200*Math.log2(e/t)
-    /*
-     */
+
     async function startTuner() {
-        const analyserNode = await $audioCtx.createAnalyser();
-        navigator.mediaDevices
-            .getUserMedia({ audio: true })
-            .then((stream) => {
-                showTuner ? (showTuner = false) : (showTuner = true);
+        if (!showTuner) {
+            const analyserNode = await $audioCtx.createAnalyser();
+            navigator.mediaDevices
+                .getUserMedia({ audio: true })
+                .then((stream) => {
+                    showTuner = true;
+                    let sourceNode = $audioCtx.createMediaStreamSource(stream);
 
-                let sourceNode = $audioCtx.createMediaStreamSource(stream);
+                    sourceNode.connect(analyserNode);
+                    const detector = PitchDetector.forFloat32Array(
+                        analyserNode.fftSize
+                    );
+                    const input = new Float32Array(detector.inputLength);
 
-                sourceNode.connect(analyserNode);
-                const detector = PitchDetector.forFloat32Array(
-                    analyserNode.fftSize
-                );
-                const input = new Float32Array(detector.inputLength);
-                updatePitch(
-                    analyserNode,
-                    detector,
-                    input,
-                    $audioCtx.sampleRate
-                );
-            })
-            .catch((e) => {
-                console.log(e);
-                alert("No Audio Source Found. Please Connect a Microphone");
-            });
+                    updatePitch(
+                        analyserNode,
+                        detector,
+                        input,
+                        $audioCtx.sampleRate
+                    );
+                })
+                .then(() => {
+                    if (Gauge) {
+                        gauge = new Gauge(gaugeDemo).setOptions(opts); // create sexy gauge!
+                        gauge.maxValue = 50; // set max gauge value
+                        gauge.setMinValue(-50); // set min value
+                        gauge.set(0); // set actual value
+                    }
+                })
+                .catch((e) => {
+                    console.log(e);
+                    alert("No Audio Source Found. Please Connect a Microphone");
+                });
+        } else if (showTuner) {
+            showTuner = false;
+        }
     }
 
     async function updatePitch(analyserNode, detector, input, sampleRate) {
         analyserNode.getFloatTimeDomainData(input);
-        let [pitch, clarity] = detector.findPitch(input, sampleRate);
+        let [pitch] = detector.findPitch(input, sampleRate);
         pitch = Math.round(pitch * 10) / 10;
-        closestPitch = await $allPitches.reduce((a, b) => {
-            return Math.abs(b.frequency - pitch) < Math.abs(a.frequency - pitch)
-                ? b
-                : a;
-        });
+        //don't continue if calculated pitch is outside of appropriate frequency range
+        if (pitch > 50 && pitch < 22000) {
+            closestPitch = await $allPitches.reduce((a, b) => {
+                return Math.abs(b.frequency - pitch) <
+                    Math.abs(a.frequency - pitch)
+                    ? b
+                    : a;
+            });
 
-        cents = Math.round(1200 * Math.log2(closestPitch.frequency / pitch));
+            cents = Math.round(
+                1200 * Math.log2(closestPitch.frequency / pitch)
+            );
+            cents < 7 && cents > -7 ? (isInTune = true) : (isInTune = false);
+            if (gauge) {
+                gauge.set(cents);
+            }
+        }
+
         if (showTuner)
             setTimeout(function () {
                 updatePitch(analyserNode, detector, input, sampleRate);
@@ -74,19 +141,15 @@
         .tuner-icon {
             width: 30px;
         }
-        .pitch {
+        .tuner-info-container {
             color: red;
             font-size: 2rem;
             &.in-tune {
                 color: green;
             }
         }
-        .pitch-dial {
-            background-image: url("../icons/needle.png");
-            width: 500px;
-            height: 500px;
-        }
     }
+
     @media only screen and (max-width: 768px) {
         .tuner-container {
             bottom: 5rem;
@@ -102,16 +165,17 @@
             class="tuner-icon"
         />
     </button>
+    <!-- Create a canvas where the tuner will be rendered -->
     {#if showTuner}
-        <div>this will be where the tuner is</div>
-        <!-- Create a canvas where the tuner will be rendered -->
-        <div class="pitch-dial" style="transform:rotate(90deg)" />
-        <div class="pitch">
-            {tunerObj.pitch}
+        <canvas id="tuner-gauge" bind:this={gaugeDemo} />
+        <div class="tuner-info-container {isInTune ? 'in-tune' : ''}">
+            <div class="pitch">
+                {tunerObj.pitch}
+            </div>
+            <div>{cents}</div>
+            {#if closestPitch}
+                <div class="note">{closestPitch.note}</div>
+            {/if}
         </div>
-        <div>{cents}</div>
-        {#if closestPitch}
-            <div>{closestPitch.note}</div>
-        {/if}
     {/if}
 </section>
