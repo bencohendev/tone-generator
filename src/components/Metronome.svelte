6740b17dd6397b8e570478a8e2fc9bd9b88d3670
bencohendev <ben@bencohen.dev>
2021-02-22 15:28:36 -0500

    working on commenting metronome

--- src/components/Metronome.svelte
+++ src/components/Metronome.svelte
@@ -3,23 +3,17 @@
     import { playFile } from "../helpers/PlayFile.svelte";
     import { audioCtx } from "../store.js";
     import { onDestroy, onMount } from "svelte";
-    import uuid from "shortid";
-    import { createNewOscillator } from "../helpers/NewOscillator.svelte";
 
     console.group("metronome");
 
-    let vol = 50;
+    let vol = 50; //start volume at halfway
     let play = false;
-    let metronome = false;
+    let showMetronome = false;
 
-    //speed and pitch range
+    //speed and number of beats
     let bpm = 60;
-    let playSpeed = 120;
     let numOfBeats = 4;
 
-    //advanced controls
-
-    //new variables
     var startTime; // The start time of the entire sequence.
     var current16thNote; // What note is currently last scheduled?
     var lookahead = 5.0; // How frequently to call scheduling function
@@ -35,11 +29,10 @@
     var unlocked = false;
     let bufferLoader;
 
-    //Create Audio Context and Oscillator
     onMount(() => {
         audioCtx.set(new (window.AudioContext || window.webkitAudioContext)());
 
-        // if we wanted to load audio files, etc., this is where we should do it.
+        // use BufferLoader to load tick audio files
         bufferLoader = new BufferLoader($audioCtx, [
             "./audio/tick.wav",
             "./audio/tick_up.wav",
@@ -47,7 +40,7 @@
 
         bufferLoader.load();
 
-        // console.log(timerWorker);
+        //create timerWorker to handle setInterval calls
         timerWorker = new Worker("./metronomeworker.js");
         timerWorker.onmessage = function (e) {
             if (e.data == "tick") {
@@ -63,9 +56,10 @@
 
     $: playHandler(play);
 
-    //new functions
-
     function nextNote() {
+        console.log("nnt: ", nextNoteTime);
+        console.log("current: ", $audioCtx.currentTime);
+        console.log("sat: ", scheduleAheadTime);
         // Advance current note and time by a 16th note...
         let secondsPerBeat = 60.0 / bpm; // Notice this picks up the CURRENT
         // bpm value to calculate beat length.
@@ -78,7 +72,7 @@
     }
 
     function scheduleNote(beatNumber, time) {
-        // push the note on the queue, even if we're not playing.
+        // push the note on the queue, even if we're not playing. for drawing
         notesInQueue.push({ note: beatNumber, time: time });
 
         if (noteResolution == 1 && beatNumber % 2) return; // we're not playing non-8th 16th notes
@@ -86,15 +80,8 @@
 
         if (!beatNumber % 16 === 0) {
             playFile($audioCtx, bufferLoader, "middle", vol);
-        }
-        // beat 0 == high pitch
-        //  console.log(bufferLoader);
-        else if (beatNumber % 4 === 0)
+        } else if (beatNumber % 4 === 0)
             playFile($audioCtx, bufferLoader, "high", vol);
-        // quarter notes = medium pitch
-        //   source2.play(time);
-        //  console.log(bufferList);
-
         // other 16th notes = low pitch
     }
 
@@ -128,7 +115,6 @@
             }
         }
     }
-
     console.groupEnd();
 </script>
 
@@ -220,7 +206,7 @@
 </style>
 
 <section class="metronome-container">
-    {#if metronome}
+    {#if showMetronome}
         <div class="metronome-container-inner">
             <div class="slide-container volume">
                 <img
@@ -306,7 +292,7 @@
     <button
         class="metronome-button"
         on:click={() => {
-            metronome ? (metronome = false) : (metronome = true);
+            showMetronome ? (showMetronome = false) : (showMetronome = true);
         }}
         ><img
             src="./icons/metronome-light.png"
